1e20ca12c87520fc5d3bebae7f07692f3320418a
Akshat Thakur <codingontheway@gmail.com>
2026-04-23 16:42:54 +0530

    2) How to insert data into database using java program (JDBC)

--- src/JDBC/JDBC_02_InsertData.java
+++ src/JDBC/JDBC_02_InsertData.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-public class JDBC_01_InsertData {
+public class JDBC_02_InsertData {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/Spark";
         String username = "root";
